965223b0bdea8d26fc06f01b0ce04e68fefd37dc
dlakra96 <lakradeepanshu96@gmail.com>
2018-03-30 23:54:16 +0530

    supply chain management

--- contract.sol
+++ contract.sol
@@ -1,30 +1,57 @@
-pragma solidity ^0.4.0;
-contract MyContract {
-    address creator;
-    uint256 myNumber;
-    funtion MyContract() public
+pragma solidity ^0.4.8;
+contract myContract{
+    address supplier_id;
+    address retailer_id;
+    string pickup_loc;
+    string date;
+    string time;
+    string curr_loc;
+    string drop_loc;
+    string comm_type;
+    uint no_of_units;
+    function setSupplier(address t)public{
+        supplier_id=t;
+    }
+    function setRetailer(address t)public{
+        retailer_id=t;
+    }
+    function placeOrder(string pick,string drop,string comm,uint nou,string curr_time,string curr_date) public
     {
-        creator=msg.sender;
-        myNumber=3;
+        date=curr_date;
+        time=curr_time;
+        pickup_loc=pick;
+        drop_loc=drop;
+        comm_type=comm;
+        no_of_units=nou;
+        curr_loc=pick;
         
     }
-    function getCreator() public constant returns(address)
+    function checkDelievery() public constant returns(bool)
     {
-        return creator;
+        if(keccak256(drop_loc)==keccak256(curr_loc))
+        return true;
+        else
+        return false;
     }
-    function getMyNumber() public constant returns(uint256)
+    function updateOrder(string curr_date,string curr_time,string loc) public
     {
-        return myNumber;
+        date=curr_date;
+        time=curr_time;
+        curr_loc=loc;
     }
-    function setMyNumber(uint256 myNewNumber) public
-    {
-        myNumber=myNewNumber;
+    function returnSupplier() public constant returns(address){
+        return supplier_id;
     }
-    function kill() public
-    {
-        if(msg.sender == creator)
-        {
-            selfdestruct(creator);
-        }
+    function returnRetailer() public constant returns(address){
+        return retailer_id;
     }
+    function orderDetails() public constant returns(string,string,string,string,uint){
+      return (date,time,curr_loc,comm_type,no_of_units);  
+    }
+    
+    
+    function () public payable{
+        
+    }
+    
 }
